5d5fa1144b7cd45f25d863de0cb25d5742ce7845
Ismaïl <nguyen.ismail@gmail.com>
2020-12-09 09:22:26 +0100

    print port on log

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
-
-const server = app.listen(process.env.PORT || 3000, function () {
-	console.log('server is listening')
+const port = process.env.PORT || 3000
+const server = app.listen(port, function () {
+	console.log('server is listening in port ' + port)
 })
 
 const io = require('socket.io')(server)
